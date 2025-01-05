c7f095f8c32bd699834fcb2cb1616ae2721e1a5f
Arata-Dev <morgan.kozyniak@gmail.com>
2025-01-04 23:11:33 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -79,3 +79,39 @@ self.addEventListener('fetch', event => {
       })
   );
 });
+
+
+
+// new code
+window.addEventListener('online', handleConnection);
+window.addEventListener('offline', handleConnection);
+
+function handleConnection() {
+  if (navigator.onLine) {
+    isReachable(getServerUrl()).then(function(online) {
+      if (online) {
+        // handle online status
+        console.log('online');
+      } else {
+        console.log('no connectivity');
+      }
+    });
+  } else {
+    // handle offline status
+    console.log('offline');
+  }
+}
+
+function isReachable(url) {
+  return fetch(url, { method: 'HEAD', mode: 'no-cors' })
+    .then(function(resp) {
+      return resp && (resp.ok || resp.type === 'opaque');
+    })
+    .catch(function(err) {
+      console.warn('[conn test failure]:', err);
+    });
+}
+
+function getServerUrl() {
+  return document.getElementById('serverUrl').value || window.location.origin;
+}
