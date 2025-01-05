2d083cd094758cf2610b8ae79e0e4d57d717181a
Arata-Dev <morgan.kozyniak@gmail.com>
2025-01-04 16:56:19 -0800

    Update apiexample.js

--- apiexample.js
+++ apiexample.js
@@ -11,6 +11,16 @@ window.onload = function() {
    };
 } // window.onload
 
+// load the service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }, function(error) {
+      console.log('Service Worker registration failed:', error);
+    });
+  });
+} 
 
 // get data from TV Maze
 async function searchTvShows() {
