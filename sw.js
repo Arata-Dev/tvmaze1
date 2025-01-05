ce7aad9deaa6522917963d7bff59ee72b2743ac7
Arata-Dev <morgan.kozyniak@gmail.com>
2025-01-04 21:53:19 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = 'v123';  // change this everytime you update the service worker
+const version = 'v5';  // change this everytime you update the service worker
                           // to force the browser to also update it.
 
 // Define cache names
@@ -8,10 +8,33 @@ const DYNAMIC_CACHE_NAME = 'my-app-dynamic-cache-v1';
 // Files to cache during install
 const STATIC_ASSETS = [
   '/index.html',
-  '/style.css',
-  '/apiexample.js',
+  '/styles.css',
+  '/app.js'
   // Add other static files you want to cache
 ];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('my-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        'index.html',
+        'style.css',
+        'apiexample.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+
 
 // Install event: Cache static assets
 self.addEventListener('install', event => {
