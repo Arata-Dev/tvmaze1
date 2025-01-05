384544a1cfaa4d645b299cfa13b7cdee5cdd4450
Arata-Dev <morgan.kozyniak@gmail.com>
2025-01-05 13:55:09 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = 'v18';  // change this everytime you update the service worker
+const version = 'v19';  // change this everytime you update the service worker
                           // to force the browser to also update it.
 
 // Define cache names
@@ -26,11 +26,30 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
+addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;     // if valid response is found in cache return it
+        } else {
+          return fetch(event.request)     //fetch from internet
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_NAME)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());    //save the response for future
+                  return res;   // return the fetched data
+                })
+            })
+            .catch(function(err) {       // fallback mechanism
+              return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
+                .then(function(cache) {
+                  // return cache.match('/offline.html');
+                  console.log("The app needs to be online for that action.");
+                });
+            });
+        }
+      })
   );
 });
 
@@ -76,7 +95,6 @@ self.addEventListener('fetch', event => {
       .catch(() => {
         // If network fetch fails, fallback to cache
         return caches.match(event.request);
-        console.log("App needs to be online.");
       })
   );
 });
