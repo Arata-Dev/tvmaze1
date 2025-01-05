33fbfce98d00390c0541fe4ff6d9043dde811a2d
Arata-Dev <morgan.kozyniak@gmail.com>
2025-01-04 21:42:18 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,6 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('my-cache').then(function(cache) {
       return cache.addAll([
-        '/',
         'index.html',
         'style.css',
         'apiexample.js'
