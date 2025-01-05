7f096586d1dee0dc4e4bd0d46c15f113ea93786e
Arata-Dev <morgan.kozyniak@gmail.com>
2025-01-05 13:04:20 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = 'v10';  // change this everytime you update the service worker
+const version = 'v11';  // change this everytime you update the service worker
                           // to force the browser to also update it.
 
 // Define cache names
@@ -83,35 +83,14 @@ self.addEventListener('fetch', event => {
 
 
 // new code
-self.addEventListener('online', handleConnection);
-self.addEventListener('offline', handleConnection);
-
-function handleConnection() {
-  if (navigator.onLine) {
-    isReachable(getServerUrl()).then(function(online) {
-      if (online) {
-        // handle online status
-        console.log('online');
-      } else {
-        console.log('no connectivity');
-      }
-    });
-  } else {
-    // handle offline status
-    console.log('offline');
+window.addEventListener("load", () => {
+  function handleNetworkChange(event) {
+    if (navigator.onLine) {
+      document.body.classList.remove("offline");
+    } else {
+      document.body.classList.add("offline");
+    }
   }
-}
-
-function isReachable(url) {
-  return fetch(url, { method: 'HEAD', mode: 'no-cors' })
-    .then(function(resp) {
-      return resp && (resp.ok || resp.type === 'opaque');
-    })
-    .catch(function(err) {
-      console.warn('[conn test failure]:', err);
-    });
-}
-
-function getServerUrl() {
-  return document.getElementById('serverUrl').value || window.location.origin;
-}
+  window.addEventListener("online", handleNetworkChange);
+  window.addEventListener("offline", handleNetworkChange);
+});
