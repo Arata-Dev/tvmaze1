1cf43a61eb5296471733df0504cbd496440c5393
Arata-Dev <morgan.kozyniak@gmail.com>
2025-01-06 00:25:18 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = 'v30';  // change this everytime you update the service worker
+const version = 'v31';  // change this everytime you update the service worker
                           // to force the browser to also update it.
 
 // Define cache names
@@ -27,31 +27,6 @@ self.addEventListener('install', function(event) {
 });
 
 
-addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;     // if valid response is found in cache return it
-        } else {
-          return fetch(event.request)     //fetch from internet
-            .then(function(res) {
-              return caches.open(DYNAMIC_CACHE_NAME)
-                .then(function(cache) {
-                  cache.put(event.request.url, res.clone());    //save the response for future
-                  return res;   // return the fetched data
-                })
-            })
-            .catch(function(err) {       // fallback mechanism
-                return caches.open('error-message.html')
-                .then(function(cache) {
-                  return cache.match('error-message.html');
-                });
-            });
-        }
-      })
-  );
-}); 
 
 
 
